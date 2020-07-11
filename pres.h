09759bbc6da040805d71ca6e4c0563dee1617548
Andrew J.C. Wilson <ajcheyneywilson@gmail.com>
2020-07-11 01:20:22 -0400

    replaced _types.h with sys/types.h for cross platform

--- pres.h
+++ pres.h
@@ -4,20 +4,20 @@
 #define PRES_H
 
 #include <stdbool.h>
-#include <_types.h>
+#include <sys/types.h>
 
 #include <SDL2/SDL.h>
 
 
 #pragma region // Types
-typedef	__int8_t		Int8;
-typedef __uint8_t		Uint8;
-typedef __int16_t		Int16;
-typedef __uint16_t		Uint16;
-typedef __int32_t		Int32;
-typedef __uint32_t		Uint32;
-typedef __int64_t		Int64;
-typedef __uint64_t		Uint64;
+typedef	int8_t			Int8;
+typedef uint8_t			Uint8;
+typedef int16_t			Int16;
+typedef uint16_t		Uint16;
+typedef int32_t			Int32;
+typedef uint32_t		Uint32;
+typedef int64_t			Int64;
+typedef uint64_t		Uint64;
 typedef float			Float32;
 typedef double			Float64;
 #pragma endregion
